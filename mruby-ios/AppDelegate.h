ad5396fbbf17cc1f80d18970efb4746385da66ae
Zachary Scott <e@zzak.io>
2015-02-27 08:44:36 -0800

    Initial Commit

---
+++ mruby-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mruby-ios
+//
+//  Created by Zachary Scott on 2/27/15.
+//  Copyright (c) 2015 Zachary Scott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mruby-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mruby-ios
+//
+//  Created by Zachary Scott on 2/27/15.
+//  Copyright (c) 2015 Zachary Scott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
